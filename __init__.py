b939f06e89e849799cf8d80be05b5caa188c229c
jiwon-center <https://github.com/jiwon-center>
2022-11-07 01:13:29 +0900

    complete login

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, redirect
-from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 import os
 import pymysql
+import datetime
 
 load_dotenv()
 
@@ -12,19 +12,10 @@ conn = pymysql.connect(host=os.environ.get('DB_URL'),
                        db=os.environ.get('DB_NAME'),
                        charset='utf8')
 
-sql = "INSERT INTO user_test (user_test_name, uset_test_num) VALUES (%s, %d)"
+sql = "INSERT INTO user (user_name, user_student_number, user_major, user_login_time) VALUES (%s, %s, %s, %s)"
 
 app = Flask(__name__)
- 
 
-nextId = 4
-Users = [
-    {'id': 1, 'Student_ID': 2020311271, 'User_name': '장지원'},
-    {'id': 2, 'Student_ID': 2018311068, 'User_name': '손승열'},
-    {'id': 3, 'Student_ID': 2019312133, 'User_name': '차은우'}
-]
- 
- 
 def template(contents, content):
     return f'''<!doctype html>
     <html>
@@ -44,8 +35,6 @@ def template(contents, content):
  
 def getContents():
     liTags = ''
-    for User in Users:
-        liTags = liTags + f'<li><h3>{User["Student_ID"]}</h3>{User["User_name"]}</li>'
     return liTags
  
  
@@ -59,25 +48,24 @@ def create():
     if request.method == 'GET': 
         content = '''
             <form action="/create/" method="POST">
-                <p><input type="int" name="Student_ID" placeholder="Student_ID"></p>
+                <p><input type="number" name="Student_ID" placeholder="Student_ID"></p>
                 <p><input type="text" name="User_name" placeholder="User_name"></p>
+                <p><input type="text" name="User_major" placeholder="User_major"></p>
                 <p><input type="submit" value="로그인"></p>
             </form>
         '''
         return template(getContents(), content)
     elif request.method == 'POST':
-        global nextId
         Student_ID = request.form['Student_ID']
         User_name = request.form['User_name']
-        newUser = {'id': nextId, 'Student_ID': Student_ID, 'User_name': User_name}
-        Users.append(newUser)
-        with conn:
-            with conn.cursor() as cur:
-                cur.execute(sql, (User_name, Student_ID))
-                conn.commit()
+        User_major = request.form['User_major']
+        User_login_time = datetime.datetime.now()
+        
+        with conn.cursor() as cur:
+            cur.execute(sql, (User_name, Student_ID, User_major, User_login_time))
+        conn.commit()
         url = '/'
-        nextId = nextId + 1
         return redirect(url)
  
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
