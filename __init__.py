a4d0bc44142fd3c5d37667fd0d248c62f7ea5df4
jiwon-center <https://github.com/jiwon-center>
2022-11-07 07:42:34 +0900

    commit

--- __init__.py
+++ __init__.py
@@ -9,6 +9,7 @@ import datetime
 load_dotenv()
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 app.secret_key = 'secretkey'
 app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=60)
 
@@ -19,6 +20,7 @@ conn = pymysql.connect(host=os.environ.get('DB_URL'),
                        charset='utf8')
 
 sql = "INSERT INTO user (user_name, user_student_number, user_major, user_login_time) VALUES (%s, %s, %s, %s)"
+like_button = 0
 
 def template(contents, content):
     return f'''<!doctype html>
@@ -60,8 +62,8 @@ def getProjects(id):
                 WHERE project_id = {id}
                 """
     with conn.cursor() as cur:
-            cur.execute(Projects)
-            project_data = cur.fetchall()
+        cur.execute(Projects)
+        project_data = cur.fetchall()
     return project_data
 
 def getTagContents(tag):
@@ -91,6 +93,8 @@ def getClassContents(class_code):
 @app.route('/')
 def index():
     if 'User_name' in session:
+        global like_button
+        like_button = 0
         return '로그인 성공! 아이디는 %s' % escape(session['User_name']) + \
             "<br><a href = '/logout'>로그아웃</a>"
 
@@ -120,7 +124,7 @@ def login():
         conn.commit()
         return redirect(url_for('index'))
 
-@app.route('/tag/')
+@app.route('/tag')
 def tag():
     tag = request.args.get('tag')
     if tag == None :
@@ -140,7 +144,7 @@ def tag():
         '''
         return templates(getTagContents(tag), content)
 
-@app.route('/class')
+@app.route('/class/')
 def class_():
     class_code = request.args.get('class')
     if class_code == None :
@@ -162,12 +166,52 @@ def class_():
 
 @app.route('/project/<int:id>/')
 def project(id):
-    Project = getProjects(id)[0]
-    title = Project[0]
-    body = Project[10]
-    return templates(getContents(), f'<h2>{title}</h2>{body}')
-    
+    Project = getProjects(id)
+    title = Project[0][0]
+    body = Project[0][10]
+    return template(getContents(), f'<h2>{title}</h2>{body}')
 
+@app.route('/project/<int:pj_id>/like/')
+def like_project(pj_id):
+    global like_button
+    if like_button == 0:
+        likeup= f"""
+                UPDATE project
+                set like_cnt = like_cnt + 1
+                where project_id = {pj_id}
+                """
+        likecnts = f"""
+                   SELECT like_cnt
+                   FROM project
+                   where project_id = {pj_id}
+                   """
+        with conn.cursor() as cur:
+            cur.execute(likeup)
+            cur.execute(likecnts)
+            like_data = cur.fetchall()
+        print(like_data[0][0])    
+        like_button = 1
+        return jsonify({'msg': '좋아요 완료!'})
+    
+    else :
+        likeup= f"""
+                UPDATE project
+                set like_cnt = like_cnt - 1
+                where project_id = {pj_id}
+                """
+        likecnts = f"""
+                   SELECT like_cnt
+                   FROM project
+                   where project_id = {pj_id}
+                   """
+        with conn.cursor() as cur:
+            cur.execute(likeup)
+            cur.execute(likecnts)
+            like_data = cur.fetchall()
+        print(like_data[0][0])    
+        like_button = 0
+        return jsonify({'msg': '좋아요 취소!'})
+    
 @app.route('/logout')
 def logout():
     session.pop('User_name', None)
