c1bbf78535fdc6e808c3dcd26ee0ef6074c9e9c3
nimod7890 <nimod1234@g.skku.edu>
2022-11-12 15:38:05 +0900

    fe: project page 구현중

--- client/src/js/Project.js
+++ client/src/js/Project.js
@@ -1,10 +1,42 @@
+// import { Axios } from 'axios';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import '../css/Class.css';
+import '../css/Project.css';
 function Project() {
-    const projectId = useParams().projectId;    
+    const projectId = useParams().projectId;   
+
+    //변수명 db에 맞게 바꿔주시면 됩니다
+    const [inputData, setInputData] = useState([{
+        className: '',
+        members: '',
+        pdf: '',
+        like: '',
+        youtubeURL:'',
+        hashtags:''
+        //더 있나 혹쉬 
+    }])
+    
+    // useEffect(async() => {
+    //     try{
+    //         // const res = await Axios.get('/api/test/{}')
+    //         // const _inputData = await res.data~~~~
+    //         // setInputData(_inputData)
+    //     } catch(e){
+    //         console.error(e.message)
+    //     }
+    // },[])
+ 
+    //더미데이터 
+    const members=['김보민','장지원','손승열','안정후'];
+    const className='DASF002I5';
+    const pdf='./ppt.pdt'  
+    const like='51';
+    const youtubeURL='https://youtu.be/ft9FP_8BLjI';
+    const hashtags=['2','1','3','4'];
+
     return (
         <div className="Project">
-            {projectId}
+            
         </div>
     );
 }
