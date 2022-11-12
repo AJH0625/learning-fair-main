880a2ce6b5b802f5905b0fc69eb4a80d89349016
nimod7890 <nimod1234@g.skku.edu>
2022-11-12 14:17:17 +0900

    fe: congrats 구현중

--- client/src/js/Congrats.js
+++ client/src/js/Congrats.js
@@ -2,7 +2,17 @@ import '../css/Congrats.css';
 function Congrats() {
     return (
         <div className="Congrats">
-            
+            <div class="CongratsWrapper">
+                <div className="first">
+                    <p>총장님</p>
+                </div>
+                <div className="second">
+                    <p>SW융합대학장님</p>
+                </div>
+                <div className="second">
+                    <p>DS교육센터장<br/>/학부대학장님</p>
+                </div>
+            </div>
         </div>
     );
 }
