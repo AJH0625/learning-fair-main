cc901711e6cf434442bd09c90c7381380b3524f9
nimod7890 <nimod1234@g.skku.edu>
2022-11-13 19:37:48 +0900

    fe: class page (분반) 구현 중

--- client/src/js/Class.js
+++ client/src/js/Class.js
@@ -1,10 +1,27 @@
 import '../css/Class.css';
 import { useParams } from 'react-router-dom';
+import Grid from './Grid/Grid';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
 function Class() {
-    const classId = useParams().classId;    
+    const classId = useParams().classId;
+    
+    useEffect(() => {
+        axios.get('/class',{class:classId})
+        .then(function (response) {
+            console.log(response)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }, []);
+
+
     return (
         <div className="Class">
-        {classId}
+            <Grid/><Grid/><Grid/>
+            <Grid/><Grid/><Grid/>
         </div>
     );
 }
---
+++ client/src/js/Grid/Grid.jsx
@@ -0,0 +1,13 @@
+function Grid({project}) {
+  return (
+    <div class="Grid">
+        <img src='{}' alt="썸네일"/>
+        <p class="GridLike">30</p>
+        <p class="GridProjectName">프로젝트 이름</p>
+        <p class="GridTeamName">팀명</p>
+        <p class="GridTeamMember">성이름, 성이름, 성이름, 성이름</p>
+        <p class="GridHashtag">#해시태그 #해시태그 #해시태그 #해시태그</p>
+    </div>
+  );
+}
+export default Grid;
