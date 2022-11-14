f56e3adc557cf9b214016bbc74afe91006f3d074
JH Ahn <0625dkswjdgn@g.skku.edu>
2022-11-15 01:49:51 +0900

    모바일 버전 햄버거 메뉴 만들었습니당

--- client/src/js/layouts/header.js
+++ client/src/js/layouts/header.js
@@ -42,7 +42,71 @@ const Header = () => {
         <Button id='CongratsBtn' onClick={()=>handleOnClick('/congrats')} value='축사'/>
         <Button id='Awards' onClick={()=>handleOnClick('/awards')} value='시상식'/>
         <Button id='Explore' onClick={()=>handleOnClick(`/project/${Math.floor( jbRandom * 100 ) }`)} value='탐험하기'/>
-      </div>  
+      </div>
+
+      <nav id="main-menu">
+        <ul>
+          <li>
+          <Button id='ClassBtn2' onClick={()=>handleOnClick('/class')} value='분반'/>
+          </li>
+          <li>
+          <Button id='TagBtn2' onClick={()=>handleOnClick('/tag')} value='해시태그'/>
+          </li>
+          <li>
+          <Button id='CongratsBtn2' onClick={()=>handleOnClick('/congrats')} value='축사'/>
+          </li>
+          <li>
+          <Button id='Awards2' onClick={()=>handleOnClick('/awards')} value='시상식'/>
+          </li>
+          <li>
+          <Button id='Explore2' onClick={()=>handleOnClick('/project')} value='탐험하기'/>
+          </li>         
+        </ul>
+      </nav>
+      <input type="checkbox" id="hamburger-input" class="burger-shower"/>
+      <label id="hamburger-menu" for="hamburger-input">
+        <nav id="sidebar-menu">
+          <h3>닫기</h3>
+          <ul>
+            <li>
+              <div class="dropdown">
+                <li>
+                <Button id='dropbtn' onClick={()=>handleOnClick('/class')} value='분반'/>
+                </li>
+                
+                <ul class="dropdown-content">
+                  <li><a href="/class/DASF002I1">DASF002I1</a></li>
+                  <li><a href="/class/DASF002I2">DASF002I2</a></li>
+                  <li><a href="/class/DASF002I3">DASF002I3</a></li>
+                  <li><a href="/class/DASF002I4">DASF002I4</a></li>
+                  <li><a href="/class/DASF002I5">DASF002I5</a></li>
+                  <li><a href="/class/DASF002I6">DASF002I6</a></li>
+                  <li><a href="/class/DASF002I7">DASF002I7</a></li>
+                  <li><a href="/class/DASF002I8">DASF002I8</a></li>
+                  <li><a href="/class/DASF002I9">DASF002I9</a></li>
+                  <li><a href="/class/GEDT015I1">GEDT015I1</a></li>
+                </ul>
+              </div>
+            </li>
+              <li>
+              <Button id='TagBtn2' onClick={()=>handleOnClick('/tag')} value='해시태그'/>
+              </li>    
+              <li>
+              <Button id='CongratsBtn2' onClick={()=>handleOnClick('/congrats')} value='축사'/>
+              </li>
+              <li>
+              <Button id='Awards2' onClick={()=>handleOnClick('/awards')} value='시상식'/>
+              </li>
+              <li>
+              <Button id='Explore2' onClick={()=>handleOnClick('/project')} value='탐험하기'/>
+              </li>             
+          </ul>
+        </nav>
+      </label>
+      <div class="overlay"></div>
+      
+
+
       <div className="headerWelcome">
         <p><span id="user">{userName}</span>님, 환영합니다!</p>
       </div>
